b82babc222a2cde4e15346f7ab4b4762513241c8
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2023-09-26 12:48:37 -0400

    Update NameCard.jsx

--- src/components/sidebar/namecard/NameCard.jsx
+++ src/components/sidebar/namecard/NameCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 import "./namecard.css";
 
 function NameCard() {
-  const isSmallScreen = useMediaQuery({
-    query: "(max-width: 1000px)",
-  });
+  // const isSmallScreen = useMediaQuery({
+  //   query: "(max-width: 1000px)",
+  // });
 
   return (
-    <a href={isSmallScreen ? "#mobile-menu" : "/"}>
+    // <a href={isSmallScreen ? "#mobile-menu" : "/"}>
       <div className="nameCard">
         <div className="name">JAMES CALINGO;</div>
         <div className="tagline">SOFTWARE ENGINEER;</div>
       </div>
-    </a>
+    // </a>
   );
 }
 
