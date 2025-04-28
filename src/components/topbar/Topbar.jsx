c4c7a2b1b4fa93d9b765b6ae16b86dfb07e94e4d
JamesCalingo <james.calingo.web@gmail.com>
2025-04-27 21:34:14 -0400

    moving resume to root

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -30,7 +30,7 @@ function Topbar() {
           <a href="https://overclocked-overthoughts.netlify.app/">Blog</a>
         </li>
         <li>
-          <a href="src/assets/jamescalingo.pdf" target="_blank" rel="noopener noreferrer">
+          <a href="/jamescalingo.pdf" target="_blank" rel="noopener noreferrer">
             Resume
           </a>
         </li>
